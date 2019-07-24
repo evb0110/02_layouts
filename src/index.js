974047acf4ec44e9e71a217f92ad5c34b4c383ff
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-07-24 21:54:58 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import products from './data/products';
+const cardsLayout = {
+  xs: 12,
+  sm: 8,
+  md: 6,
+  lg: 4
+};
+
+ReactDOM.render(
+  <App layout={cardsLayout} products={products} />,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
